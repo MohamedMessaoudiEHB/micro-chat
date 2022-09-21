206199129e3b47ebb9738509d97eeffb47718fc6
MohamedMessaoudiEHB <114060847+MohamedMessaoudiEHB@users.noreply.github.com>
2022-09-21 22:27:24 +0900

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        music.playMelody("C5 B A G F E D C ", 0)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
